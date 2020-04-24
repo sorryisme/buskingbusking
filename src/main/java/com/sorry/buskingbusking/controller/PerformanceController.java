b337f5a92c39687ac67430553e3d45090fa4a30a
sorryisme <wivipp39@gmail.com>
2020-04-24 20:33:24 +0900

    20200424 @todo - Member Login 진행예정

--- src/main/java/com/sorry/buskingbusking/controller/PerformanceController.java
+++ src/main/java/com/sorry/buskingbusking/controller/PerformanceController.java
@@ -1,11 +1,25 @@
 package com.sorry.buskingbusking.controller;
 
+import com.sorry.buskingbusking.domain.Notice;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @Slf4j
 @AllArgsConstructor
 public class PerformanceController {
+
+
+    @GetMapping("/performance/list")
+    public String movePagePerformance(Model model){
+        return "/performance/performanceList";
+    }
+
+
 }
